8323f34e7abe2d91082e275109eee627f0210d23
Denis <denis360_93@mail.ru>
2020-02-18 21:21:33 +0300

    init: Add material-ui and theme context

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import AuthorList from './components/AuthorsList';
-import Main from './components/Main';
-import AuthorPage from './components/AuthorPage';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import AuthorList from './components/AuthorsList'
+import Main from './components/Main'
+import AuthorPage from './components/AuthorPage'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './config/material.theme'
 
-ReactDOM.render((
+ReactDOM.render(
   <BrowserRouter>
-    <App>
-      <Switch>
-        <Route exact path='/' component={Main} />
-        <Route exact path='/authors' component={AuthorList} />
-        <Route path='/authors/:id' component={AuthorPage} />
-      </Switch>
-    </App>
-  </BrowserRouter>
-  ), document.getElementById('root')
+    <ThemeProvider theme={theme}>
+      <App>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/authors" component={AuthorList} />
+          <Route path="/authors/:id" component={AuthorPage} />
+        </Switch>
+      </App>
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 )
