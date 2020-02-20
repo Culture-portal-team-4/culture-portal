f7c52dbde29da71460305bc4ff8e817ade26b840
Lenocka <lvutina.en@gmail.com>
2020-02-20 17:42:34 +0300

    feat: TASK-5 add info about 1 author (without dynamic id)
    https://trello.com/c/FOer8vYZ/12-task-5author-information-component

--- src/components/author-information/AuthorInformation.jsx
+++ src/components/author-information/AuthorInformation.jsx
@@ -1,62 +1,34 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import data from '../../data/data.json';
+import {useParams} from "react-router-dom";
 import styles from './author-ingormation-style';
-import { Typography, IconButton } from '@material-ui/core';
-import MenuIcon from "@material-ui/icons/Menu";
-//import MenuIcon from '@material-ui/icons/Menu';
+import { Grid, Typography, Box } from '@material-ui/core';
+import Container from "@material-ui/core/Container";
 
 
-const AuthorInformation = ({ handleOpenMenu }) => {
-    const classes = styles()
-
+export default function AuthorInformation() {
+    const { photographers } = data;
+    const classes = styles();
+    //const id = useParams();
     return (
-        <header class={classes.header}>
-            <div class={classes.headerContainer}>
-                <div className={classes.boxLeft}>
-                    <IconButton onClick={handleOpenMenu}>
-                        <MenuIcon />
-                    </IconButton>
-                </div>
-                <div className={classes.boxRight}>
-                    <div className={classes.logoText}>
-                        <Typography variant="h4">Photographers of belarus</Typography>
-                    </div>
-                </div>
-            </div>
-        </header>
+        <Container>
+            <Grid item container direction="column" alignItems="center" sm={12}>
+                    <Box mb={5} mt={5} >
+                        <img alt="Author Photo" src={photographers[0].img} className={classes.photo} />
+                    </Box>
+                    <Box  color="text.title" mb={2} >
+                        <Typography variant="h4" component="h1">
+                            {photographers[0].name}
+                        </Typography>
+                    </Box>
+
+                <Box color="text.primary" mb={2} >
+                    <Typography variant="h6" component="h2">
+                    {photographers[0].yearsOfLife}
+                    </Typography>
+                </Box>
+            </Grid>
+        </Container>
+
     )
 }
-
-AuthorInformation.propTypes = {
-    select: PropTypes.string
-}
-
-export default AuthorInformation
-
-
-const Header = ({ handleOpenMenu }) => {
-    const classes = styles()
-
-    return (
-        <header class={classes.header}>
-            <div class={classes.headerContainer}>
-                <div className={classes.boxLeft}>
-                    <IconButton onClick={handleOpenMenu}>
-                        <MenuIcon />
-                    </IconButton>
-                </div>
-                <div className={classes.boxRight}>
-                    <div className={classes.logoText}>
-                        <Typography variant="h4">Photographers of belarus</Typography>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
-}
-
-Header.propTypes = {
-    select: PropTypes.string
-}
-
-export default Header
--- src/components/author-information/author-ingormation-style.js
+++ src/components/author-information/author-ingormation-style.js
@@ -1,38 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const style = makeStyles(theme => ({
-    header: {
-        // position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '80px',
+
+    photo: {
+        //position: 'fixed',
+
+        width: '220px',
+        height: '180px',
         zIndex: 302
-    },
-    headerContainer: {
-        display: 'flex',
-        height: '100%',
-        borderBottom: `1px solid ${theme.palette.secondary.border}`
-    },
-    boxLeft: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: theme.spacing(3),
-        borderRight: `1px solid ${theme.palette.secondary.border}`
-    },
-    boxRight: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1
-    },
-    logoText: {
-        background: `linear-gradient(135deg, ${theme.palette.secondary.gradientStart} 20%, ${theme.palette.secondary.gradientEnd} 70%)`,
-        textFillColor: 'transparent',
-        WebkitBackgroundClip: 'text',
-        textTransform: 'uppercase'
     }
+
 }))
 
 export default style
