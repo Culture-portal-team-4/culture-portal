54b4ce5dcc95e10bff116ff6370cdef761201ba6
Lenocka <lvutina.en@gmail.com>
2020-02-24 17:22:01 +0300

    end:[TASK-18] add photo gallery

--- src/components/AuthorPage.js
+++ src/components/AuthorPage.js
@@ -1,13 +1,13 @@
 import React, {Fragment} from 'react';
 import AuthorInformation from "./author-information/AuthorInformation";
-import MyGallery from "./author-information/image-gallery/ImageGallery";
+import PhotoGallery from "./author-information/image-gallery/ImageGallery";
 
 const Author = () => {
 
   return (
       <Fragment>
         <AuthorInformation />
-        <MyGallery/>
+        <PhotoGallery/>
       </Fragment>
 
   );
--- src/components/author-information/image-gallery/ImageGallery.jsx
+++ src/components/author-information/image-gallery/ImageGallery.jsx
@@ -1,34 +1,42 @@
-
 import React, {Fragment} from 'react';
 import ImageGallery from 'react-image-gallery';
 import data from '../../../data/data.json';
 import './image-gallery.css';
 import Typography from "@material-ui/core/Typography";
-import {Box} from "@material-ui/core";
+import {Grid} from "@material-ui/core";
+import {useParams} from "react-router-dom";
+import PropTypes from "prop-types";
 const imageSet = (id=0)=>{
     let images= data.photographers[id].gallery.map((work)=>{
-        let imageObject = new Object ()
+        let imageObject = {};
         imageObject.original = work.link;
         imageObject.thumbnail = work.link;
         return imageObject
     });
    return images;
 
-}
+};
 
-export default class MyGallery extends React.Component {
-    render() {
+PhotoGallery.propTypes = {
+    id: PropTypes.number
+};
+PhotoGallery.defaultProps = {
+    id: 0,
+
+};
+export default function PhotoGallery(props) {
+    const {id} = useParams();
         return (
                 <Fragment>
-                    <Box  color="text.title" mb={2} >
+                    <Grid item container direction="column" alignItems="center" sm={12}>
                         <Typography variant="h4" component="h1">
                             {'Gallery of works'}
                         </Typography>
-                    <ImageGallery items={imageSet(0)} />
-                    </Box>
+                    <ImageGallery items={imageSet(id)} description = {'Lolita'}/>
+                    </Grid>
                 </Fragment>
             )
 
 
-    }
+
 }
